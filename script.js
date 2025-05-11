c9643a5348a5b4375f8fb442019671c982606514
ixsmina <romivaleri12@gmail.com>
2025-05-11 16:51:48 -0600

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const menuToggle = document.getElementById('menu-toggle');
+<scrip>
+    const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active');
+  function toggleAlbum(id) {
+    const section = document.getElementById(id);
+    section.classList.toggle('hidden');
+  }
+</script>
+
 });
