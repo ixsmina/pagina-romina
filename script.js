c28dfa64e199e718710a5d0f7e5ee22b1ae73025
ixsmina <romivaleri12@gmail.com>
2025-05-11 17:00:35 -0600

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 <script>
-    const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-menuToggle.addEventListener('click', () => {
+  // Menú hamburguesa
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
+
+  menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active');
+  });
+
+  // Mostrar detalles del álbum
   function toggleAlbum(id) {
     const section = document.getElementById(id);
     section.classList.toggle('hidden');
   }
 </script>
-                            });
