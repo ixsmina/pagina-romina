7310a5430ef2a49f5500e09fa059d97da4287a25
ixsmina <romivaleri12@gmail.com>
2025-05-27 01:55:47 -0600

    Update alb.js

--- alb.js
+++ alb.js
@@ -1,13 +1,13 @@
-@@ -3,10 +3,10 @@ const toggle = document.getElementById('menu-toggle');
+// Menú hamburguesa en móvil
+const toggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 
 toggle.addEventListener('click', () => {
-  menu.classList.toggle('show');
   menu.classList.toggle('hidden');
 });
 
-// Función para mostrar u ocultar la lista de canciones
 // Mostrar u ocultar la lista de canciones
 function toggleAlbum(id) {
   const list = document.getElementById(id);
   list.classList.toggle('hidden');
+}
